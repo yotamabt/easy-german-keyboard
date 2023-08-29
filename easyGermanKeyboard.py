9cac55b3756f6344e02d319b98ab7a3459e650ce
Yotam Abt <Yotam.Abt@mako.co.il>
2023-08-29 21:34:44 +0300

    fix language change

--- easyGermanKeyboard.py
+++ easyGermanKeyboard.py
@@ -8,6 +8,7 @@ import sys
 import os
 import subprocess
 from tkinter import messagebox
+import ctypes
 
 processes = subprocess.Popen('tasklist', stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE).communicate()[0]
 
@@ -17,7 +18,6 @@ dev_mode = False
 if processes.decode().count(os.path.basename(sys.executable)) > 2  and not dev_mode:
     messagebox.showerror('App already running!', f'{os.path.basename(sys.executable)} already running')
     sys.exit(0)
-
 
 def resource_path(relative_path):
 
@@ -29,9 +29,52 @@ def resource_path(relative_path):
 
     return os.path.join(base_path, relative_path)
 
+def get_languages():
+    with open(resource_path('language_dict' ), 'r') as f:
+        return ({x[-1].strip() : x[0] for x in [r.split('		') for r in f.read().split('\n')]})
+
+
+def get_keyboard_language():
+    languages = get_languages()
+
+    user32 = ctypes.WinDLL('user32', use_last_error=True)
+
+    handle = user32.GetForegroundWindow()
+
+    threadid = user32.GetWindowThreadProcessId(handle, 0)
+
+    layout_id = user32.GetKeyboardLayout(threadid)
+
+    language_id = layout_id & (2 ** 16 - 1)
+
+    language_id_hex = hex(language_id)
+
+    if language_id_hex in languages.keys():
+        return languages[language_id_hex]
+    else:
+        return str(language_id_hex)
+
+kbl = get_keyboard_language()
+
 
 kbd = Controller()
 shift_pressed = False
+def switch_language():
+    kbd.press(Key.alt_l)
+    kbd.press(Key.shift)
+    kbd.release(Key.alt_l)
+    kbd.release(Key.shift)
+if 'English' not in kbl and 'German' not in kbl:
+    switch_language()
+    if not dev_mode:
+        subprocess.Popen(sys.executable)
+        sys.exit()
+    else:
+        subprocess.Popen([sys.executable , __file__])
+        sys.exit()
+
+
+
 
 def on_activate_a():
     if not shift_pressed:
